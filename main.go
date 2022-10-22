7e9d39cc2243f9e7bdd116de70fc0113229d0b27
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-22 20:35:53 +0000

    feat: :sparkles: setup Mux Server and FX App
    Create DI container with Uber FX package and setup Gorilla Mux server with GraphQL routes.

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/dan6erbond/jamboree-api/pkg"
+	"github.com/spf13/viper"
+)
+
+func main() {
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	viper.SetDefault("server.port", 5001)
+	viper.SetDefault("server.host", "127.0.0.1")
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	app := pkg.NewApp()
+	app.Run()
+}
