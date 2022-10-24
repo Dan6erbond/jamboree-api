cfb0eb02b052734535a7e081de3b67cd926d89c3
RaviAnand Mohabir <moravrav@gmail.com>
2022-10-24 08:35:24 +0000

    feat: :sparkles: add support for environment variable app configuration and set defaults for db port

--- main.go
+++ main.go
@@ -15,6 +15,18 @@ func main() {
 	viper.SetDefault("server.port", 5001)
 	viper.SetDefault("server.host", "0.0.0.0")
 
+	viper.SetDefault("db.port", 5432)
+
+	viper.AutomaticEnv()
+
+	viper.BindEnv("db.host", "DB_HOST", "POSTGRES_HOST")
+	viper.BindEnv("db.port", "DB_PORT", "POSTGRES_PORT")
+	viper.BindEnv("db.user", "DB_USER", "POSTGRES_USER")
+	viper.BindEnv("db.password", "DB_PASSWORD", "POSTGRES_PASSWORD")
+	viper.BindEnv("db.database", "DB_DATABASE", "POSTGRES_DATABASE")
+
+	viper.BindEnv("environment", "GO_ENV")
+
 	err := viper.ReadInConfig()
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %w", err))
